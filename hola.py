0e4257c80082643ce8e1fdec3bb310f47bac46f4
RodrigoDominguezJimenez <zs21015978@estudiantes.uv.mx>
2022-10-21 08:17:24 -0500

    Se agregó código de interfaz

--- hola.py
+++ hola.py
@@ -1,5 +1,28 @@
-print('Hola Mundo')
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QLineEdit, QPushButton, QWidget
 
-print('mundo hola')
 
-print('LAHO DOMUN')
+class VentanaPrincipal(QMainWindow):
+    # constructor
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Peliculas piratas")
+        layout = QHBoxLayout()
+
+        buscar = QHBoxLayout()
+
+        self.bbuscar = QLineEdit()
+        buscar.addWidget(self.bbuscar)
+        self.bbuscar = QPushButton("Buscar")
+        buscar.addWidget(self.bbuscar)
+
+        layout.addLayout(buscar)
+
+        contenedor = QWidget()
+        contenedor.setLayout(layout)
+        self.setCentralWidget(contenedor)
+
+
+app = QApplication([])
+ventana = VentanaPrincipal()
+ventana.show()
+app.exec_()
