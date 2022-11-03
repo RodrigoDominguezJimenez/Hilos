5c7409f8275a8dc057cfeb1a4f00963cefb4d883
CarlosAlbertoTamarizMorales <zS21015980@estudiantes.uv.mx>
2022-11-02 19:40:02 -0600

    Un poco mas de cambios buenos

--- hola.py
+++ hola.py
@@ -1,7 +1,8 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QLabel ,QSizePolicy, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QGridLayout, QWidget, QFrame
+from PyQt5.QtCore import Qt,pyqtSignal,QTimer
+from PyQt5.QtWidgets import QApplication, QLabel, QSizePolicy, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QGridLayout, QWidget, QFrame
 import requests
 from PyQt5.QtGui import QImage, QPixmap
+
 
 # Sub-clase que hereda de QMainWindow
 class VentanaPrincipal(QMainWindow):
@@ -17,34 +18,38 @@ class VentanaPrincipal(QMainWindow):
 
         self.texto = QLineEdit()
         self.button = QPushButton("Buscar")
+        self.infor = QPushButton("informacion")
         self.image = QImage()
         self.image_label = QLabel()
-        self.url_image = []
+        self.image_label2 = QLabel()
+        self.image_label3 = QLabel()
+        self.url_image = None
+        self.id = None
+        self.title = None
+        self.description = None
 
         self.setup_ui()
 
     def setup_ui(self):
         self.setWindowTitle('uvFlix')
-        self.setSizePolicy(500,400)
+        self.resize(500,350)
 
-        self.texto.setFixedWidth(100)
+        self.texto.setFixedWidth(400)
         self.texto.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         self.button.clicked.connect(lambda: self.search_url())
+        #self.infor.clicked.connect(lambda: self.click)
 
         self.superior.addWidget(self.texto)
         self.superior.addWidget(self.button)
-        self.center.addWidget(self.image_label)
+        self.center.addWidget(self.image_label, 1, 1)
+        self.center.addWidget(self.image_label2, 1, 2)
+        self.center.addWidget(self.image_label3, 1, 3)
+        self.center.addWidget(self.infor)
 
         self.center1.addLayout(self.superior)
         self.center1.addLayout(self.center)
         self.container.setLayout(self.center1)
-
-        url = self.url_image[1]
-        self.image.loadFromData(requests.get(url).content)
-        pixmap = QPixmap(self.image)
-        pixmap2 = pixmap.scaledToWidth(200)
-        self.image_label.setPixmap(pixmap2)
 
         self.setCentralWidget(self.container)
 
@@ -55,8 +60,17 @@ class VentanaPrincipal(QMainWindow):
         data = r.json()
         data2 = data['results']
         data3 = data2[0:3]
-        for i in data3:
-            self.url_image.append(i['image'])
+        self.url_image = data3[1]['image']
+        self.id = data3[1]['id']
+        self.title = data3[1]['title']
+        self.description = data3[1]['description']
+        self.image.loadFromData(requests.get(self.url_image).content)
+        pixmap = QPixmap(self.image)
+        pixmap2 = pixmap.scaledToWidth(200)
+        self.image_label.setPixmap(pixmap2)
+
+    def click(self):
+        self.image_label.show()
 
 
 if __name__ == '__main__':
