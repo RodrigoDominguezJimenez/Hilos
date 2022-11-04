6763f0c4242ecdd348005af14b28c12d740b81e2
CarlosAlbertoTamarizMorales <zS21015980@estudiantes.uv.mx>
2022-11-03 18:25:45 -0600

    un cambio 10/10

--- hola.py
+++ hola.py
@@ -1,12 +1,10 @@
-from PyQt5.QtCore import Qt,pyqtSignal,QTimer
 from PyQt5.QtWidgets import QApplication, QLabel, QSizePolicy, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QGridLayout, QWidget, QFrame
 import requests
 from PyQt5.QtGui import QImage, QPixmap
+import threading
 
-
-# Sub-clase que hereda de QMainWindow
 class VentanaPrincipal(QMainWindow):
-    # constructor
+
     def __init__(self):
         super().__init__()
         self.container = QWidget()
@@ -14,38 +12,58 @@ class VentanaPrincipal(QMainWindow):
 
         self.superior = QHBoxLayout()
         self.center = QGridLayout()
+        #self.center = QHBoxLayout()
         self.center1 = QVBoxLayout()
 
         self.texto = QLineEdit()
         self.button = QPushButton("Buscar")
-        self.infor = QPushButton("informacion")
-        self.image = QImage()
-        self.image_label = QLabel()
-        self.image_label2 = QLabel()
-        self.image_label3 = QLabel()
-        self.url_image = None
+        self.infor = [QPushButton("informacion"),
+                      QPushButton("informacion"),
+                      QPushButton("informacion"),
+                      QPushButton("informacion"),
+                      QPushButton("informacion")
+                      ]
+        self.image = [QImage(),
+                      QImage(),
+                      QImage(),
+                      QImage(),
+                      QImage()]
+        self.image_label = [QLabel(),
+                            QLabel(),
+                            QLabel(),
+                            QLabel(),
+                            QLabel()]
+        self.url_image = []
         self.id = None
         self.title = None
         self.description = None
+        self.thread = threading.Thread(target=self.search_url)
 
         self.setup_ui()
 
     def setup_ui(self):
         self.setWindowTitle('uvFlix')
-        self.resize(500,350)
+        self.resize(1000,350)
 
-        self.texto.setFixedWidth(400)
+        self.texto.setFixedWidth(800)
         self.texto.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
-        self.button.clicked.connect(lambda: self.search_url())
+        self.button.setFixedWidth(50)
+        self.button.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed))
+
+        self.button.clicked.connect(lambda: self.thread.start())
         #self.infor.clicked.connect(lambda: self.click)
 
         self.superior.addWidget(self.texto)
         self.superior.addWidget(self.button)
-        self.center.addWidget(self.image_label, 1, 1)
-        self.center.addWidget(self.image_label2, 1, 2)
-        self.center.addWidget(self.image_label3, 1, 3)
-        self.center.addWidget(self.infor)
+        x=0
+        for i in self.image_label:
+            self.center.addWidget(i,1,x)
+            x = x+1
+        #self.center.addWidget(self.image_label, 1, 0)
+        #self.center.addWidget(self.image_label2, 1, 2)
+        #self.center.addWidget(self.image_label3, 1, 3)
+       # self.center.addWidget(self.infor)
 
         self.center1.addLayout(self.superior)
         self.center1.addLayout(self.center)
@@ -55,19 +73,39 @@ class VentanaPrincipal(QMainWindow):
 
     def search_url(self):
         d = 'https://clandestina-hds.com/movies/title?search='
-        t = d + self.texto.text()
-        r = requests.get(t)
-        data = r.json()
-        data2 = data['results']
-        data3 = data2[0:3]
-        self.url_image = data3[1]['image']
-        self.id = data3[1]['id']
-        self.title = data3[1]['title']
-        self.description = data3[1]['description']
-        self.image.loadFromData(requests.get(self.url_image).content)
-        pixmap = QPixmap(self.image)
-        pixmap2 = pixmap.scaledToWidth(200)
-        self.image_label.setPixmap(pixmap2)
+        movies = self.texto.text().split(sep=',', maxsplit=5)
+        t = []
+        for h in movies:
+            t.append(d+h)
+
+        r = []
+
+        for i in t:
+            r.append(requests.get(i))
+
+        data = []
+
+        for q in r:
+            data.append(q.json())
+
+        data2 = []
+        for v in data:
+            data2.append(v['results'])
+
+        data3 = []
+
+        for n in data2:
+            data3.append(n[0:3])
+
+        for k in data3:
+            self.url_image.append(k[1]['image'])
+        x=0
+        for m in self.image:
+            m.loadFromData(requests.get(self.url_image[x]).content)
+            pixmap = QPixmap(m)
+            pixmap2 = pixmap.scaledToWidth(200)
+            self.image_label[x].setPixmap(pixmap2)
+            x = x + 1
 
     def click(self):
         self.image_label.show()
