c93877d869c57ae2478b31e404fae6b057d67ea4
CarlosAlbertoTamarizMorales <zS21015980@estudiantes.uv.mx>
2022-11-03 22:55:50 -0600

    y se acabo

--- hola.py
+++ hola.py
@@ -1,9 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QSizePolicy, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, \
-    QHBoxLayout, QGridLayout, QWidget, QFrame
+from PyQt5.QtWidgets import QApplication, QLabel, QSizePolicy, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QGridLayout, QWidget, QFrame
 import requests
 from PyQt5.QtGui import QImage, QPixmap
 import threading
-
 
 class VentanaPrincipal(QMainWindow):
 
@@ -17,7 +15,7 @@ class VentanaPrincipal(QMainWindow):
         self.center = QGridLayout()
         self.center1 = QVBoxLayout()
 
-        self.container_two = QWidget()
+        self.container_two= QWidget()
         self.center_two = QVBoxLayout()
 
         self.texto = QLineEdit()
@@ -44,19 +42,23 @@ class VentanaPrincipal(QMainWindow):
         self.title_label = QLabel()
         self.description = []
         self.description_label = QLabel()
+        self.runTime = []
+        self.runTime_label = QLabel()
+        self.plot = []
+        self.plot_label = QLabel()
         self.thread = threading.Thread(target=self.search_url)
 
         self.setup_ui()
 
     def setup_ui(self):
         self.setWindowTitle('uvFlix')
-        self.resize(1000, 350)
+        self.resize(1000,350)
 
         self.texto.setFixedWidth(800)
         self.texto.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         self.button.setFixedWidth(50)
-        self.button.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.button.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed))
 
         self.button.clicked.connect(lambda: self.thread.start())
         self.infor.clicked.connect(lambda: self.click(0))
@@ -67,11 +69,11 @@ class VentanaPrincipal(QMainWindow):
 
         self.superior.addWidget(self.texto)
         self.superior.addWidget(self.button)
-        x = 0
+        x=0
         for i in self.image_label:
-            self.center.addWidget(i, 1, x)
-            x = x + 1
-        self.center.addWidget(self.infor, 2, 0)
+            self.center.addWidget(i,1,x)
+            x = x+1
+        self.center.addWidget(self.infor,2,0)
         self.center.addWidget(self.infor2, 2, 1)
         self.center.addWidget(self.infor3, 2, 2)
         self.center.addWidget(self.infor4, 2, 3)
@@ -88,7 +90,7 @@ class VentanaPrincipal(QMainWindow):
         movies = self.texto.text().split(sep=',', maxsplit=5)
         t = []
         for h in movies:
-            t.append(d + h)
+            t.append(d+h)
 
         r = []
 
@@ -108,14 +110,16 @@ class VentanaPrincipal(QMainWindow):
 
         for n in data2:
             data3.append(n[0:3])
-        y = 0
+        y=0
         for k in data3:
             self.url_image.append(k[0]['image'])
             self.id.append(k[0]['id'])
             self.title.append(k[0]['title'])
             self.description.append(k[0]['description'])
-            y = y + 1
-        x = 0
+            self.runTime.append(k[0]['runtimeStr'])
+            self.plot.append(k[0]['plot'])
+            y= y+1
+        x=0
         for m in self.image:
             if x != y:
                 m.loadFromData(requests.get(self.url_image[x]).content)
@@ -124,7 +128,7 @@ class VentanaPrincipal(QMainWindow):
                 self.image_label[x].setPixmap(pixmap2)
                 x = x + 1
 
-    def click(self, num):
+    def click(self,num):
         x = num
         self.windowTwo.setWindowTitle('Informacion')
 
@@ -137,14 +141,28 @@ class VentanaPrincipal(QMainWindow):
         description = 'Description: ' + self.description[x]
         self.description_label.setText(description)
 
+        runTime = 'Run time: ' + self.runTime[x]
+        self.runTime_label.setText(runTime)
+
+        plot = 'Plot: ' + self.plot[x]
+        self.plot_label.setText(plot)
+
         self.center_two.addWidget(self.id_label)
         self.center_two.addWidget(self.title_label)
         self.center_two.addWidget(self.description_label)
+        self.center_two.addWidget(self.runTime_label)
+        self.center_two.addWidget(self.plot_label)
+
 
         self.container_two.setLayout(self.center_two)
 
         self.windowTwo.setCentralWidget(self.container_two)
         self.windowTwo.show()
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -153,4 +171,4 @@ if __name__ == '__main__':
     window.show()
     app.exec_()
 
-    # moana, jumanji, frozen, Big Hero 6, Inside Out
+    #moana, jumanji, frozen, Big Hero 6, Inside Out
